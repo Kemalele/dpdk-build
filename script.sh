eff3fffab490edd665aa77c9f35f5db487c5a55d
k.bairamov <k.bairamov@kazdream.kz>
2021-04-22 19:12:08 +0600

    add build script

---
+++ script.sh
@@ -0,0 +1,13 @@
+mkdir dpdk;
+cd dpdk;
+git clone https://github.com/yerden/dpdk-build.git
+chmod +x dpdk-build/scripts/pre-build.ubuntu.sh;
+./dpdk-build/scripts/pre-build.ubuntu.sh;
+wget https://fast.dpdk.org/rel/dpdk-20.11.1.tar.xz;
+tar xJf dpdk-20.11.1.tar.xz;
+cd dpdk-stable-20.11.1;
+pip3 install meson
+meson build;
+cd build;
+ninja;
+ninja install;
